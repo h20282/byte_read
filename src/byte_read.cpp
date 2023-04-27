0036ca0fb39ef57c1a0d65256d8c0f604d779433
郭智侯 <guozhihou@cvte.com>
2023-04-27 10:23:50 +0800

    [feature] add line_width

--- src/byte_read.cpp
+++ src/byte_read.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using std::cin;
@@ -12,29 +13,31 @@ void PutLine(const std::vector<uint8_t> &line) {
     printf("  |  ");
     for (auto c : line) {
         if (std::isprint(c)) {
-            printf(" %c ", c);
+            printf("%c", c);
         } else {
-            printf(" * ");
+            printf("*");
         }
     }
 }
 
 int main(int argc, char **argv) {
     std::ifstream ifs(argv[1], std::ios::in | std::ios::binary);
+    int line_width = 16;
+    if (argc >= 3) { line_width = std::stoi(argv[2]); }
     uint8_t c;
     int cnt = 0;
     std::vector<uint8_t> line;
     while (ifs.read(reinterpret_cast<char *>(&c), sizeof(c))) {
         printf("%02x ", c);
         line.push_back(c);
-        if (++cnt % 16 == 0) {
+        if (++cnt % line_width == 0) {
             PutLine(line);
             line.clear();
             cout << endl;
         }
     }
     if (!line.empty()) {
-        int n = 16 - line.size();
+        int n = line_width - line.size();
         while (n--) { printf("   "); }
         PutLine(line);
     }
